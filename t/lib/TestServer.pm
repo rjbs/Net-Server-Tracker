bd067ad4ccdd13810c872b0f09188770d545eb64
Ricardo Signes <rjbs@cpan.org>
2016-05-20 15:59:34 -0400

    remove a reminder of older, signatures-using code!
    😢

--- t/lib/TestServer.pm
+++ t/lib/TestServer.pm
@@ -4,7 +4,7 @@ package TestServer;
 use parent 'Net::Server::Tracker', 'Net::Server::PreFork';
 
 my $n = 0;
-sub process_request ($self) {
+sub process_request {
   my ($self) = @_;
   $self->update_tracking("handled request number " . $n++);
   return;
